02609c667f6d6d681225f1bdbdc656a57e264180
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-02-22 09:48:12 +1100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// random generated number
+
+const entryElm = document.getElementById("entry");
+const pwElm = document.getElementById("pw");
+
+const randomNumber = (max) => {
+  return Math.floor(Math.random() * max + 1);
+};
+
+const handleOnClick = () => {
+  let lottoEntry = [];
+  //   let lottoEntry = " ";
+  for (let i = 0; lottoEntry.length < 7; i++) {
+    const numb = randomNumber(35);
+
+    if (!lottoEntry.includes(numb)) lottoEntry.push(numb);
+    // lottoEntry += numb + " ";
+  }
+  entryElm.innerText = lottoEntry;
+
+  pwElm.innerText = randomNumber(20);
+};
